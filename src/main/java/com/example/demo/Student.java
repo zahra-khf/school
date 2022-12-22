4125e115397a117a823f035f487b7a2d1874a8d3
zkhalifa <zkhalifa@et.esiea.fr>
2022-12-23 00:53:36 +0100

    create data base

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,83 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import javax.sound.midi.Sequence;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    Long id ;
+    @Column
+    String first_name ;
+    @Column
+    String last_name ;
+    @Column
+    String email ;
+    @Column
+    Integer age ;
+
+    public Student(Long id, String first_name, String last_name, String email, Integer age) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
